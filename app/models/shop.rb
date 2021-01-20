410cb34b41e92e2143517d05787019f56119e99f
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2021-01-20 10:03:25 +0900

    Add validation for shop.rb

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,8 +1,7 @@
 class Shop < ApplicationRecord
   validates :name, presence: true
-  validates :postal_code, presence: true
+  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
   validates :prefecture, presence: true
   validates :city, presence: true
-  validates :building_name, presence: true
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, format: {with: /\A\d{10,11}\z/}
 end
