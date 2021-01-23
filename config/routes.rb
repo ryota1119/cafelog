7e48310cbb95c47532de24830db140b0d1e5d7b6
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2021-01-23 21:13:21 +0900

    Edit owners registrations & sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :owners, skip: :all
-  devise_scope :owner do
-    get 'owners/sign_in' => 'owners/sessions#new', as: 'new_owner_session'
-    post 'owners/sign_in' => 'owners/sessions#create', as: 'owner_session'
-    delete 'owners/sign_out' => 'owners/sessions#destroy', as: 'destroy_owner_session'
-    get 'owners/sign_up' => 'owners/registrations#new', as: 'new_owner_registration'
-    post 'owners' => 'owners/registrations#create', as: 'owner_registration'
-    get 'owners/password/new' => 'owners/passwords#new', as: 'new_owner_password'
-  end
+  devise_for :owners, controllers: {
+    sessions:      'owners/sessions',
+    passwords:     'owners/passwords',
+    registrations: 'owners/registrations'
+  }
   namespace :owners do
     resources :shops
   end
+  get 'home/show'
   root "home#index"
 end
