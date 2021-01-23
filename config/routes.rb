96dccc15e0c024d2597c9b3fd68e21a6fac63517
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2021-01-23 15:48:16 +0900

    Create owners model by devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :owners, controller: {
+    sessions: 'owners/sessions'
+  }
   get 'home/index'
   get 'home/show'
-  root "home#index"
+  root "shops#index"
   resources :shops
 end
