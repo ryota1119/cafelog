5a561649c67c5114c326134f4ad5ee63d982af93
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2021-02-25 21:55:29 +0900

    Mark up owner page

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,4 +1,13 @@
 class OwnersController < ApplicationController
+  before_action :set_owner, only: [:show]
+  
   def show
+    @shops = Shop.where(owner_id: params[:id])
+  end
+
+  private
+
+  def set_owner
+    @owner = Owner.find(params[:id])
   end
 end
